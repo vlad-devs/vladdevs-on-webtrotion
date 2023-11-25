4a127c7539cefd9de59b7d435c00b16caba31315
nerdymomocat <nerdymomocat@gmail.com>
2023-11-24 16:37:41 -0800

    Update code in various files

--- astro.config.ts
+++ astro.config.ts
@@ -7,52 +7,94 @@ import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import remarkUnwrapImages from "remark-unwrap-images";
 import { remarkReadingTime } from "./src/utils/remark-reading-time";
+import { astroImageTools } from "astro-imagetools";
+
+import satori from "astro-satori";
+
+import { CUSTOM_DOMAIN, BASE_PATH } from "./src/constants";
+// import CoverImageDownloader from './src/integrations/cover-image-downloader';
+// import CustomIconDownloader from './src/integrations/custom-icon-downloader';
+// import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
+// import PublicNotionCopier from './src/integrations/public-notion-copier';
+
+const getSite = function () {
+  if (CUSTOM_DOMAIN) {
+    return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
+  }
+
+  if (process.env.VERCEL && process.env.VERCEL_URL) {
+    return new URL(BASE_PATH, `https://${process.env.VERCEL_URL}`).toString();
+  }
+
+  if (process.env.CF_PAGES) {
+    if (process.env.CF_PAGES_BRANCH !== 'main') {
+      return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
+    }
+
+    return new URL(
+      BASE_PATH,
+      `https://${new URL(process.env.CF_PAGES_URL).host
+        .split('.')
+        .slice(1)
+        .join('.')}`
+    ).toString();
+  }
+
+  return new URL(BASE_PATH, 'http://localhost:4321').toString();
+};
 
 // https://astro.build/config
 export default defineConfig({
   // ! Please remember to replace the following site property with your own domain
   //FIXME: what do I do here and how to directly import it?
-  site: "https://astro-cactus.chriswilliams.dev/",
+  // site: "https://astro-cactus.chriswilliams.dev/",
+  site: getSite(),
+  base: BASE_PATH,
   markdown: {
     remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-    remarkRehype: { footnoteLabelProperties: { className: [""] } },
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: [""]
+      }
+    },
     shikiConfig: {
       theme: "dracula",
-      wrap: true,
-    },
+      wrap: true
+    }
   },
   integrations: [
     // mdx({}),
     tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    prefetch(),
+      applyBaseStyles: false
+    }), sitemap(), prefetch(), astroImageTools, satori(),
+    // CoverImageDownloader(),
+    // CustomIconDownloader(),
+    // FeaturedImageDownloader(),
+    // PublicNotionCopier()
   ],
   image: {
-    domains: ["webmention.io"],
+    domains: ["webmention.io"]
   },
   vite: {
     plugins: [rawFonts([".ttf"])],
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-  },
+      exclude: ["@resvg/resvg-js"]
+    }
+  }
 });
-
 function rawFonts(ext: Array<string>) {
   return {
     name: "vite-plugin-raw-fonts",
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore:next-line
     transform(_, id) {
-      if (ext.some((e) => id.endsWith(e))) {
+      if (ext.some(e => id.endsWith(e))) {
         const buffer = fs.readFileSync(id);
         return {
           code: `export default ${JSON.stringify(buffer)}`,
-          map: null,
+          map: null
         };
       }
-    },
+    }
   };
 }
