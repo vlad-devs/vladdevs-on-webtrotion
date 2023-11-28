ebf6eefad6c1e9059ca21308496c0f36efda8036
nerdymomocat <nerdymomocat@gmail.com>
2023-11-28 03:21:54 -0800

    Update color classes in style-helpers.ts and tailwind.config.ts to add exact notion colors but added a more vibrant version of notion's dark background colors

--- src/lib/style-helpers.ts
+++ src/lib/style-helpers.ts
@@ -6,27 +6,27 @@ export const convertToTailwindColor = (s: string) => {
   // Unified mapping of custom colors to Tailwind classes for text and background colors
   const colorMap = {
     // Text color classes
-    'gray': 'text-gray-800 dark:text-gray-300',
-    'brown': 'text-amber-800 dark:text-amber-300', // No exact brown in Tailwind, using yellow
-    'orange': 'text-orange-800 dark:text-orange-300',
-    'yellow': 'text-yellow-800 dark:text-yellow-300',
-    'green': 'text-emerald-800 dark:text-emerald-300',
-    'blue': 'text-sky-800 dark:text-sky-300',
-    'purple': 'text-purple-800 dark:text-purple-300',
-    'pink': 'text-pink-800 dark:text-pink-300',
-    'red': 'text-red-800 dark:text-red-300',
+    'gray': 'text-ngray-txt-light dark:text-ngray-txt-dark',
+    'brown': 'text-nbrown-txt-light dark:text-nbrown-txt-dark',
+    'orange': 'text-norange-txt-light dark:text-norange-txt-dark',
+    'yellow': 'text-nyellow-txt-light dark:text-nyellow-txt-dark',
+    'green': 'text-ngreen-txt-light dark:text-ngreen-txt-dark',
+    'blue': 'text-nblue-txt-light dark:text-nblue-txt-dark',
+    'purple': 'text-npurple-txt-light dark:text-npurple-txt-dark',
+    'pink': 'text-npink-txt-light dark:text-npink-txt-dark',
+    'red': 'text-nred-txt-light dark:text-nred-txt-dark',
     'default': '',
 
     // Background color classes
-    'gray-background': 'px-1 rounded bg-gray-50 dark:bg-gray-900',
-    'brown-background': 'px-1 rounded bg-amber-50 dark:bg-amber-900', // No exact brown in Tailwind, using amber
-    'orange-background': 'px-1 rounded bg-orange-50 dark:bg-orange-900',
-    'yellow-background': 'px-1 rounded bg-yellow-50 dark:bg-yellow-900',
-    'green-background': 'px-1 rounded bg-emerald-50 dark:bg-emerald-900',
-    'blue-background': 'px-1 rounded bg-sky-50 dark:bg-sky-900',
-    'purple-background': 'px-1 rounded bg-purple-50 dark:bg-purple-900',
-    'pink-background': 'px-1 rounded bg-pink-50 dark:bg-pink-500',
-    'red-background': 'px-1 rounded bg-red-50 dark:bg-red-900',
+    'gray-background': 'px-1 rounded bg-ngray-bg-light dark:bg-ngray-bg-dark',
+    'brown-background': 'px-1 rounded bg-nbrown-bg-light dark:bg-nbrown-bg-dark',
+    'orange-background': 'px-1 rounded bg-norange-bg-light dark:bg-norange-bg-dark',
+    'yellow-background': 'px-1 rounded bg-nyellow-bg-light dark:bg-nyellow-bg-dark',
+    'green-background': 'px-1 rounded bg-ngreen-bg-light dark:bg-ngreen-bg-dark',
+    'blue-background': 'px-1 rounded bg-nblue-bg-light dark:bg-nblue-bg-dark',
+    'purple-background': 'px-1 rounded bg-npurple-bg-light dark:bg-npurple-bg-dark',
+    'pink-background': 'px-1 rounded bg-npink-bg-light dark:bg-npink-bg-dark',
+    'red-background': 'px-1 rounded bg-nred-bg-light dark:bg-nred-bg-dark',
     'default-background': '',
   };
 
@@ -41,16 +41,16 @@ export const convertToTailwindColorForBorder = (s: string) => {
   // Unified mapping of custom colors to Tailwind classes for text and background colors
   const colorMap = {
     // Background color classes
-    'gray-background': 'border-gray-200 dark:border-gray-700',
-    'brown-background': 'border-amber-200 dark:border-amber-700', // No exact brown in Tailwind, using amber
-    'orange-background': 'border-orange-200 dark:border-orange-700',
-    'yellow-background': 'border-yellow-200 dark:border-yellow-700',
-    'green-background': 'bg-emerald-200 dark:border-emerald-700',
-    'blue-background': 'border-sky-200 dark:border-sky-700',
-    'purple-background': 'border-purple-200 dark:border-purple-700',
-    'pink-background': 'border-pink-200 dark:border-pink-700',
-    'red-background': 'border-red-200 dark:border-red-700',
-    'default-background': 'border-gray-200 dark:border-gray-700',
+    'gray-background': 'border-gray-200 dark:border-gray-800',
+    'brown-background': 'border-amber-200 dark:border-amber-800', // No exact brown in Tailwind, using amber
+    'orange-background': 'border-orange-200 dark:border-orange-800',
+    'yellow-background': 'border-yellow-200 dark:border-yellow-800',
+    'green-background': 'bg-emerald-200 dark:border-emerald-800',
+    'blue-background': 'border-sky-200 dark:border-sky-800',
+    'purple-background': 'border-purple-200 dark:border-purple-800',
+    'pink-background': 'border-pink-200 dark:border-pink-800',
+    'red-background': 'border-red-200 dark:border-red-800',
+    'default-background': 'border-gray-200 dark:border-gray-800',
   };
 
   // Return the Tailwind color classes, defaulting to the input if no mapping is found
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,7 @@ export default {
     textOpacity: false,
     fontVariantNumeric: false,
   },
+
   theme: {
     extend: {
       colors: {
@@ -35,6 +36,69 @@ export default {
         accent: "rgb(var(--theme-accent) / <alpha-value>)",
         "accent-2": "rgb(var(--theme-accent-2) / <alpha-value>)",
         quote: "rgb(var(--theme-quote) / <alpha-value>)",
+        ngray: {
+          'txt-light': '#9B9A97',
+          'txt-dark': '#979A9B',
+          'bg-light': '#EBECED',
+          // 'bg-dark': '#454B4E',
+          'bg-dark': '#606E72'
+        },
+        nbrown: {
+          'txt-light': '#64473A',
+          'txt-dark': '#937264',
+          'bg-light': '#ede4df',
+          // 'bg-dark': '#434040',
+          'bg-dark': '#5C5653'
+        },
+        norange: {
+          'txt-light': '#D9730D',
+          'txt-dark': '#FFA344',
+          'bg-light': '#FAEBDD',
+          // 'bg-dark': '#594A3A',
+          'bg-dark': '#7D5E4C'
+        },
+        nyellow: {
+          'txt-light': '#DFAB01',
+          'txt-dark': '#FFDC49',
+          'bg-light': '#FBF3DB',
+          // 'bg-dark': '#59563B',
+          'bg-dark': '#7D7A4C'
+        },
+        ngreen: {
+          'txt-light': '#0F7B6C',
+          'txt-dark': '#4DAB9A',
+          'bg-light': '#DDEDEA',
+          // 'bg-dark': '#354C4B',
+          'bg-dark': '#4C7D7A'
+        },
+        nblue: {
+          'txt-light': '#0B6E99',
+          'txt-dark': '#529CCA',
+          'bg-light': '#DDEBF1',
+          // 'bg-dark': '#364954',
+          'bg-dark': '#4C6D7D'
+        },
+        npurple: {
+          'txt-light': '#6940A5',
+          'txt-dark': '#9A6DD7',
+          'bg-light': '#EAE4F2',
+          // 'bg-dark': '#443F57',
+          'bg-dark': '#625C7A'
+        },
+        npink: {
+          'txt-light': '#AD1A72',
+          'txt-dark': '#E255A1',
+          'bg-light': '#F4DFEB',
+          // 'bg-dark': '#533B4C',
+          'bg-dark': '#7D4C6D'
+        },
+        nred: {
+          'txt-light': '#E03E3E',
+          'txt-dark': '#FF7369',
+          'bg-light': '#FBE4E4',
+          // 'bg-dark': '#594141',
+          'bg-dark': '#7D4C4C'
+        },
       },
       fontFamily: {
         // Add any custom fonts here
